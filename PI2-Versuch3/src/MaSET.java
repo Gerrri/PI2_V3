8957b23c6a0d3d2beb0bc41d30190cc6d16d723d
Gerrri <dustin-a@gmx.de>
2017-05-29 16:22:42 +0200

    Initial Commit

---
+++ PI2-Versuch3/src/MaSET.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.TreeSet;
+
+
+public class MaSET {
+	TreeSet<MaschineS>tmenge;
+	HashSet<MaschineS>hmenge;
+	
+	int dat2tset(BufferedReader br1)throws IOException{
+		
+		return 0;
+	}
+	
+	
+	
+	int dat2hset(BufferedReader br1)throws IOException{
+		
+		return 0;
+	}
+	
+	
+	String[] set2String(int a){
+		String[] Str_Ar;
+		
+		
+		
+		
+		
+		
+		
+		
+		return Str_Ar;
+	}
+}
