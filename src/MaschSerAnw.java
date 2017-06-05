4a00b6aa39d91b79fed6cd130d505fbcf5001f99
Gerrri <dustin-a@gmx.de>
2017-06-05 15:04:34 +0200

    Händischer Merg

--- src/MaschSerAnw.java
+++ src/MaschSerAnw.java
@@ -1,5 +1,7 @@
-import java.awt.List;
 import java.io.*;
+import java.util.*;
+
+
 
 public class MaschSerAnw {
 	
@@ -8,15 +10,25 @@ public class MaschSerAnw {
 		BufferedReader pr1 = new BufferedReader(pe1);
 	}
 	
-	static int maschSerAus(BufferedReader br1,ObjectOutputStream ow1){
+	static int maschSerAus(BufferedReader br1, ObjectOutputStream ow1) throws IOException{
+		int zaehler = 0;
+		String csv = br1.readLine();
+		while (csv!=null){
+			MaschineSer temp = new MaschineSer(csv);
+			ow1.writeObject(temp);
+			csv = br1.readLine();
+			zaehler++;
+		}
 		
 		
-		
-		return 0;
+		return zaehler;
 	}
 
-	static int maschSerEIN(ObjectInputStream or1,List<MaschineSer> xs3){
-		
+	static int maschSerEIN(ObjectInputStream or1, List<MaschineSer> xs3) throws IOException, ClassNotFoundException{
+		MaschineSer temp;
+		temp = (MaschineSer)or1.readObject();
+		xs3.add(temp);
+		or1.close();
 		return 0;
 	}
 	
