2b5441ef56fafe24f43fbccfdac469f0e1d7d836
DarthBaluu <axel.lammertz@gmx.de>
2017-06-06 09:51:58 +0200

    170606 09:51 Axel A8 eingerückt

--- src/MaschSerAnw.java
+++ src/MaschSerAnw.java
@@ -1,21 +1,19 @@
 import java.io.*;
 import java.util.*;
 
-
-
 public class MaschSerAnw {
-	
-	public static void main (String[] args) throws IOException, ClassNotFoundException{	
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		FileReader pe1 = new FileReader("MASCH.TXT");
 		BufferedReader pr1 = new BufferedReader(pe1);
 		OutputStream os1 = new FileOutputStream("OS.txt");
 		ObjectOutputStream os2 = new ObjectOutputStream(os1);
 		InputStream is1 = new FileInputStream("OS.txt");
 		ObjectInputStream is2 = new ObjectInputStream(is1);
-		List<MaschineSer> xs3= new LinkedList<MaschineSer>();
+		List<MaschineSer> xs3 = new LinkedList<MaschineSer>();
 		int output = maschSerAus(pr1, os2);
 		int input = maschSerEIN(is2, xs3);
-		for (int i =0; i< xs3.size(); i++){
+		for (int i = 0; i < xs3.size(); i++) {
 			System.out.print(xs3.get(i).getManr());
 			System.out.print(";");
 			System.out.print(xs3.get(i).getPreis());
@@ -27,44 +25,33 @@ public class MaschSerAnw {
 			System.out.println("");
 		}
 	}
-	
-	static int maschSerAus(BufferedReader br1, ObjectOutputStream ow1) throws IOException, ClassNotFoundException{
+
+	static int maschSerAus(BufferedReader br1, ObjectOutputStream ow1) throws IOException, ClassNotFoundException {
 		int zaehler = 0;
 		String csv = br1.readLine();
-		while (csv!=null){
+		while (csv != null) {
 			MaschineSer temp = new MaschineSer(csv);
 			ow1.writeObject(temp);
 			csv = br1.readLine();
 			zaehler++;
 		}
-		
-		
+
 		return zaehler;
 	}
 
-	static int maschSerEIN(ObjectInputStream or1, List<MaschineSer> xs3) throws IOException, ClassNotFoundException{
+	static int maschSerEIN(ObjectInputStream or1, List<MaschineSer> xs3) throws IOException, ClassNotFoundException {
 		MaschineSer temp;
-		int i =0;
-		/*temp = (MaschineSer)or1.readObject();
-		while (temp != null){
-		xs3.add(temp);
-		temp = (MaschineSer)or1.readObject();
+		int i = 0;
+		try {
+			while (true) {
+				temp = (MaschineSer) or1.readObject();
+				i++;
+				xs3.add(temp);
+			}
+		} catch (EOFException eo1) {
+			or1.close();
+			return i;
 		}
-		or1.close();*/
-		try{while(true)
-		{temp=(MaschineSer)or1.readObject();
-		i++;
-		xs3.add(temp);
-		}
-		}
-		catch(EOFException eo1)
-		{
-		or1.close();
-		return i;
+
 	}
-	
-	
-	
-	
-	
-}}
+}
